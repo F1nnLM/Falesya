5ba707861ac630f3d79496e122d1e5c15c038358
kebi <113300950+F1nnLM@users.noreply.github.com>
2026-03-23 09:59:28 +0100

    0.1.1
    Modificati i models

--- apps/interazioni/models.py
+++ apps/interazioni/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from apps.falesie.models import Falesia, Percorso
+
+class Commento(models.Model):
+    utente = models.ForeignKey(User, on_delete=models.CASCADE)
+    falesia = models.ForeignKey(Falesia, on_delete=models.CASCADE, null=True, blank=True)
+    percorso = models.ForeignKey(Percorso, on_delete=models.CASCADE, null=True, blank=True)
+    testo = models.TextField()
+    data = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.utente.username} - {self.data:%d/%m/%Y}"
+
+
+class Valutazione(models.Model):
+    utente = models.ForeignKey(User, on_delete=models.CASCADE)
+    falesia = models.ForeignKey(Falesia, on_delete=models.CASCADE, null=True, blank=True)
+    percorso = models.ForeignKey(Percorso, on_delete=models.CASCADE, null=True, blank=True)
+    voto = models.IntegerField()
+
+class Preferito(models.Model):
+    utente = models.ForeignKey(User, on_delete=models.CASCADE)
+    falesia = models.ForeignKey(Falesia, on_delete=models.CASCADE, null=True, blank=True)
+    percorso = models.ForeignKey(Percorso, on_delete=models.CASCADE, null=True, blank=True)
